cf34ae675ad9556d7cf242ad90ec44a02824ed1a
jonsax6 <jdc.saxjunky@gmail.com>
2020-10-14 18:52:40 -0400

    changed new kitten id assignment to 1st kitty id = 1

--- contracts/Kittycontract.sol
+++ contracts/Kittycontract.sol
@@ -61,7 +61,7 @@ contract Kittycontract is IERC721, Ownable {
             generation: uint16(_generation)
         });
 
-        uint256 newKittenId = kitties.push(_kitty) -1;
+        uint256 newKittenId = kitties.push(_kitty);
 
         emit Birth(_owner, newKittenId, _momId, _dadId, _genes);
 
