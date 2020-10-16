3827599c0031a0587d5ac95391b2e734e595cbdb
jonsax6 <jdc.saxjunky@gmail.com>
2020-10-15 22:44:08 -0400

    WIP approvals in Kittycontract

--- contracts/Kittycontract.sol
+++ contracts/Kittycontract.sol
@@ -99,6 +99,40 @@ contract Kittycontract is IERC721, Ownable {
         return userTokenIds;
     } 
 
+    /// @notice Change or reaffirm the approved address for an NFT
+    /// @dev The zero address indicates there is no approved address.
+    ///  Throws unless `msg.sender` is the current NFT owner, or an authorized
+    ///  operator of the current owner.
+    /// @param _approved The new approved NFT controller
+    /// @param _tokenId The NFT to approve
+    function approve(address _approved, uint256 _tokenId) external{
+        require(kittyIndexToOwner[_tokenId] == msg.sender);
+        kittyIndexToApproved[_tokenId] = _approved;    
+    }
+
+
+    /// @notice Enable or disable approval for a third party ("operator") to manage
+    ///  all of `msg.sender`'s assets
+    /// @dev Emits the ApprovalForAll event. The contract MUST allow
+    ///  multiple operators per owner.
+    /// @param _operator Address to add to the set of authorized operators
+    /// @param _approved True if the operator is approved, false to revoke approval
+    function setApprovalForAll(address _operator, bool _approved) external {
+        
+    }
+
+    /// @notice Get the approved address for a single NFT
+    /// @dev Throws if `_tokenId` is not a valid NFT.
+    /// @param _tokenId The NFT to find the approved address for
+    /// @return The approved address for this NFT, or the zero address if there is none
+    function getApproved(uint256 _tokenId) external view returns (address);
+
+    /// @notice Query if an address is an authorized operator for another address
+    /// @param _owner The address that owns the NFTs
+    /// @param _operator The address that acts on behalf of the owner
+    /// @return True if `_operator` is an approved operator for `_owner`, false otherwise
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+
     function balanceOf(address owner) external view returns (uint256 balance) 
     {
         return ownershipTokenCount[owner];
