d275ed075a2f57b49a663a141f59f6b314ce5b72
jonsax6 <jdc.saxjunky@gmail.com>
2020-12-21 10:02:10 -0500

    switched to Kovan testnet

--- client/index.js
+++ client/index.js
@@ -8,12 +8,12 @@ var user;
 // var marketplaceAddress = "0x777Ad6549e2a1fd15142cF1794e02Baa161be659";
 
 // kovan
-// var contractAddress = "0x760395caeeB7347BAe3ac1B6ddaA0fde6e21a5c0";
-// var marketplaceAddress = "0x868c19f002A4a01a15FA5571B19ff72966a5e4bb";
+var contractAddress = "0x760395caeeB7347BAe3ac1B6ddaA0fde6e21a5c0";
+var marketplaceAddress = "0x868c19f002A4a01a15FA5571B19ff72966a5e4bb";
 
 // ganache
-var contractAddress = "0x530b7c6cBc89c83b21Dcf6701cD7c7F5Af63242c";
-var marketplaceAddress = "0xaA9c2F96F26749E27bDC8D86b553b9eF2fd15c12";
+// var contractAddress = "0x530b7c6cBc89c83b21Dcf6701cD7c7F5Af63242c";
+// var marketplaceAddress = "0xaA9c2F96F26749E27bDC8D86b553b9eF2fd15c12";
 
 var tokenIds;
 var catObj;
@@ -178,7 +178,7 @@ async function buyCat(id, price) {
 // combines two cats DNA to make a child cat. This all happens ETH contract-side and is saved to the blockchain.
 function breedCats(_dadId, _momId, grid){
     instance.methods
-    .breed(_dadId, _momId)
+    .breed(_dadId, _momId, _seed)
     .send()
     .on("transactionHash", function (hash) {
         console.log(hash); // 'etherscan.io/tx/${hash}'
